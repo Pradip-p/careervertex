2ba544b364ed48f758fc90c7dca82a27f313936e
Pradip-p <thapapradip542@gmail.com>
2024-06-15 14:47:44 +0545

    fix: remove jeklins

--- deployment/jenkins_deploy_prod_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ssh root@159.69.172.93 <<EOF
-  cd projects/django/job-portal/
-  git pull
-  docker-compose -f docker-compose.prod.yml up --build -d
-  exit
-EOF
--- jobs/settings.py
+++ jobs/settings.py
@@ -14,7 +14,7 @@ env = environ.Env()
 
 SECRET_KEY = "@pzqp#x^+#(olu#wy(6=mi9&a8n+g&x#af#apn07@j=5oin=xb"
 
-DEBUG = env("DEBUG", default=True)
+DEBUG = env("DEBUG", default=False)
 
 SITE_ID = 1
 INSTALLED_APPS = [
