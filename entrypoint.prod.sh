79417b8d79f9bfd27920680a05c58be1167fa92e
Pradip-p <thapapradip542@gmail.com>
2024-06-16 09:29:47 +0545

    fix: update docker file

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -7,11 +7,5 @@ done
 
 echo "PostgreSQL is up and running!"
 
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Apply database migrations
-python manage.py makemigrations jobsapp resume_cv accounts tags oauth2_provider
-echo "PostgreSQL is up and running!"
 # Continue with your Django application startup command
 exec "$@"
